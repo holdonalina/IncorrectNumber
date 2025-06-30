36872096fd1e0e1314582b69db8987530eb97320
holdonalina <elli.born1@gmail.com>
2025-06-30 15:37:55 +0200

    Fix

--- InvalidNumbers/Program.cs
+++ InvalidNumbers/Program.cs
@@ -1,30 +1,29 @@
 ﻿Console.WriteLine("Enter the first number:");
-bool numberA = double.TryParse(Console.ReadLine(), out double a);
+  string numberA = Console.ReadLine();
 
+  Console.WriteLine("Enter the operator(+, -, * or /):");
+  string operators = Console.ReadLine();
 
-Console.WriteLine("Enter the operator(+, -, * or /):");
-string operators = Console.ReadLine();
-
-Console.WriteLine("Enter the second number:");
-bool numberB = double.TryParse(Console.ReadLine(), out double b);
+  Console.WriteLine("Enter the second number:");
+  string numberB = Console.ReadLine();
 
 try
 {
-  if (!numberA || !numberB)
-  {
-    throw new FormatException("Incorrect symbols");
-  }
+    int a = int.Parse(numberA);
+    int b = int.Parse(numberB);
 
   double result = operators switch
-  {
-    "+" => a + b,
-    "-" => a - b,
-    "*" => a * b,
-    "/" => a / b,
-  };
-  Console.WriteLine($"{a} {operators} {b} = {result}");
+    {
+        "+" => a + b,
+        "-" => a - b,
+        "*" => a * b,
+        "/" => a / b,
+    };
+
+    Console.WriteLine($"{a} {operators} {b} = {result}");
 }
 catch (FormatException)
 {
-  Console.WriteLine("Incorrect symbols");
+    Console.WriteLine("Invalid symbols");
 }
+
